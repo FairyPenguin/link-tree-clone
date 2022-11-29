3c7258bb47d60cfe2c5e6d9f397bd83df6c536f8
FairyPenguin <autorun81@gmail.com>
2022-11-30 00:03:42 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const shareButtons = document.querySelectorAll(".tile-share-button");
+
+console.log(shareButtons);
+
+async function copyText(e) {
+  e.preventDefault();
+  const link = this.getAttribute("link");
+  console.log(link);
+  try {
+    await navigator.clipboard.writeText(link);
+    alert("Copied the text: " + link);
+  } catch (error) {
+    err;
+  }
+}
+
+shareButtons.forEach((shareButtons) =>
+  shareButtons.addEventListener("click", copyText)
+);
